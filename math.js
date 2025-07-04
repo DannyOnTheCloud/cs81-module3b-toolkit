5dda3b4b5eb573348669d7e1fd9b69f1389eb25a
Daniel Solis <backupdirects@gmail.com>
2025-07-04 11:39:53 -0700

    Update math.js
    Writing Math Functions.

--- math.js
+++ math.js
@@ -1,1 +1,25 @@
+//This function multiplies the input by 2 
+function double(num) {
+  return num * 2;
+}
+
+// This function squares a number
+function square(num) {
+  return num * num;
+}
+
+//This function checks if the number is even
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+// This function checks if the number is odd
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+
+// This function multiplies two numbers
+function multiply(a, b) {
+  return a * b;
+}
 
